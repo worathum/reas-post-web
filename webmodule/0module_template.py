8ee83f6fe1d7ebe7c290a866d71628c7a20ee84f
arnut <kla.arnut@gmail.com>
2020-03-31 23:30:44 +0700

    update module template

--- webmodule/0module_template.py
+++ webmodule/0module_template.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import lib_httprequest
+from .lib_httprequest import *
 from bs4 import BeautifulSoup
 import os.path
-from urlparse import urlparse
+# from urlparse import urlparse
 import re
 import json
 import datetime
+import sys
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import time
+
+httprequestObj = lib_httprequest()
 
 
-class websitename():
+class module_websitename():
 
-    name = 'websitename'
+    name = 'module_websitename'
 
     def __init__(self):
 
@@ -22,120 +30,340 @@ class websitename():
 
         self.encoding = 'utf-8'
         self.imgtmp = 'imgtmp'
-        self.primarydomain = ''
         self.debug = 0
         self.debugresdata = 0
 
-    def register_user(self, userdata):
+        self.options = Options()
+        self.options.add_argument("--headless")  # Runs Chrome in headless mode.
+        self.options.add_argument('--no-sandbox')  # Bypass OS security model
+        self.options.add_argument('start-maximized')
+        self.options.add_argument('disable-infobars')
+        self.options.add_argument("--disable-extensions")
+        self.options.add_argument("window-size=1024,768")
+        self.chromedriver_binary = "/bin/chromedriver"
+
+    def register_user(self, postdata):
         self.print_debug('function ['+sys._getframe().f_code.co_name+']')
         time_start = datetime.datetime.utcnow()
 
-        email_user = userdata['email_user']
-        email_pass = userdata['email_pass']
-        company_name = userdata['company_name']
-        name_title = userdata["name_title"]
-        name_th = userdata["name_th"]
-        surname_th = userdata["surname_th"]
-        name_en = userdata["name_en"]
-        surname_en = userdata["surname_en"]
-        tel = userdata["tel"]
-        addr_province = userdata["addr_province"]
+        # start process
+        #
+        datahandled = self.postdata_handle(postdata)
+        success = "true"
+        detail = ""
 
         #
-        #
-        #
-
-        return {
-            "success": "true",
-            "time_usage": time_end - time_start,
-            "time_start": time_start,
-            "time_end": time_end,
-            "detail": ""
-        }
-
-    def test_login(self, logindata):
-        self.print_debug('function ['+sys._getframe().f_code.co_name+']')
-        time_start = datetime.datetime.utcnow()
-
-        email_user = logindata['email_user']
-        email_pass = logindata['email_pass']
-
-        #
-        #
-        #
-
-        return {
-            "success": "true",
-            "time_usage": time_end - time_start,
-            "time_start": time_start,
-            "time_end": time_end,
-            "detail": ""
-        }
-
-    def create_post(self, postdata, webdata):
-        self.print_debug('function ['+sys._getframe().f_code.co_name+']')
-        time_start = datetime.datetime.utcnow()
-
-        listing_type = postdata['listing_type']
-        property_type = postdata['property_type']
-        post_img_url_lists = postdata['post_img_url_lists']
-        price_baht = postdata['price_baht']
-        addr_province = postdata['addr_province']
-        addr_district = postdata['addr_district']
-        addr_sub_district = postdata['addr_sub_district']
-        addr_road = postdata['addr_road']
-        addr_near_by = postdata['addr_near_by']
-        floorarea_sqm = postdata['floorarea_sqm']
-        geo_latitude = postdata['geo_latitude']
-        geo_longitude = postdata['geo_longitude']
-        property_id = postdata['property_id']
-        post_title_th = postdata['post_title_th']
-        post_description_th = postdata['post_description_th']
-        post_title_en = postdata['post_title_en']
-        post_description_en = postdata['post_description_en']
-        ds_id = webdata["ds_id"]
-        account_type = webdata["account_type"]
-        user = webdata["user"]
-        password = webdata["pass"]
-        project_name = webdata["project_name"]
-
-        #
-        #
-        #
+        # end process
 
         time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
         return {
-            "success": "true",
-            "time_usage": time_end - time_start,
-            "time_start": time_start,
-            "time_end": time_end,
-            "ds_id": "4",
-            "post_url": "http://xxxxx/post/232323",
-            "post_id": "33333",
-            "account_type": null,
-            "detail": ""
+            "success": register_success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "detail": detail,
+        }
+
+    def test_login_httpreq(self, postdata):
+        self.print_debug('function ['+sys._getframe().f_code.co_name+']')
+        time_start = datetime.datetime.utcnow()
+
+        # start process
+        #
+
+        #
+        # end process
+
+        time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
+        return {
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "detail": detail,
+            "agent_id": agent_id
+        }
+
+    def test_login_headless(self, postdata):
+        self.print_debug('function ['+sys._getframe().f_code.co_name+']')
+        time_start = datetime.datetime.utcnow()
+
+        # start process
+        #
+
+        #
+        # end process
+
+        time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
+        return {
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "detail": detail,
+            "agent_id": agent_id
+        }
+
+    def test_login(self, postdata):
+        self.print_debug('function ['+sys._getframe().f_code.co_name+']')
+        time_start = datetime.datetime.utcnow()
+
+        # start process
+        #
+        datahandled = self.postdata_handle(postdata)
+        success = "true"
+        detail = ""
+        agent_id = ""
+
+        #
+        # condition such as
+        # login by test_login_headless OR test_login_httpreq
+        # default http req
+        #
+
+        #
+        # end process
+
+        time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
+        return {
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "detail": detail,
+            "agent_id": agent_id
+        }
+
+    def postdata_handle(self, postdata):
+        self.print_debug('function ['+sys._getframe().f_code.co_name+']')
+
+        datahandled = {}
+
+        # "SALE", "RENT", "OPT" ขาย ให้เช่า ขายดาวน์
+        try:
+            datahandled['listing_type'] = postdata['listing_type']
+        except KeyError:
+            datahandled['listing_type'] = "SALE"
+        if datahandled['listing_type'] == "ให้เช่า":
+            datahandled['listing_type'] = "RENT"
+        elif datahandled['listing_type'] == "ขายดาวน์":
+            datahandled['listing_type'] = "OPT"
+        else:
+            datahandled['listing_type'] = "SALE"
+
+        # "CONDO","BUNG","TOWN","LAND","APT","RET","OFF","WAR","BIZ","SHOP"]
+        try:
+            datahandled['property_type'] = postdata['property_type']
+        except KeyError:
+            datahandled['property_type'] = "CONDO"
+        if datahandled['property_type'] == 2 or datahandled['property_type'] == "บ้านเดี่ยว":
+            datahandled['property_type'] = "BUNG"
+        elif datahandled['property_type'] == 3 or datahandled['property_type'] == "บ้านแฝด":
+            datahandled['property_type'] = "BUNG"
+        elif datahandled['property_type'] == 4 or datahandled['property_type'] == "ทาวน์เฮ้าส์":
+            datahandled['property_type'] = "TOWN"
+        elif datahandled['property_type'] == 5 or datahandled['property_type'] == "ตึกแถว-อาคารพาณิชย์":
+            datahandled['property_type'] = "SHOP"
+        elif datahandled['property_type'] == 6 or datahandled['property_type'] == "ที่ดิน":
+            datahandled['property_type'] = "LAND"
+        elif datahandled['property_type'] == 7 or datahandled['property_type'] == "อพาร์ทเมนท์":
+            datahandled['property_type'] = "APT"
+        elif datahandled['property_type'] == 8 or datahandled['property_type'] == "โรงแรม":
+            datahandled['property_type'] = "BIZ"
+        elif datahandled['property_type'] == 9 or datahandled['property_type'] == "ออฟฟิศสำนักงาน":
+            datahandled['property_type'] = "OFF"
+        elif datahandled['property_type'] == 10 or datahandled['property_type'] == "โกดัง-โรงงาน":
+            datahandled['property_type'] = "WAR"
+        else:
+            datahandled['property_type'] = "CONDO"
+
+        try:
+            datahandled['post_img_url_lists'] = postdata['post_img_url_lists']
+        except KeyError:
+            datahandled['post_img_url_lists'] = {}
+
+        try:
+            datahandled['price_baht'] = postdata['price_baht']
+        except KeyError:
+            datahandled['price_baht'] = 0
+
+        try:
+            datahandled['addr_province'] = postdata['addr_province']
+        except KeyError:
+            datahandled['addr_province'] = ''
+
+        try:
+            datahandled['addr_district'] = postdata['addr_district']
+        except KeyError:
+            datahandled['addr_district'] = ''
+
+        try:
+            datahandled['addr_sub_district'] = postdata['addr_sub_district']
+        except KeyError:
+            datahandled['addr_sub_district'] = ''
+
+        try:
+            datahandled['addr_road'] = postdata['addr_road']
+        except KeyError:
+            datahandled['addr_road'] = ''
+
+        try:
+            datahandled['addr_near_by'] = postdata['addr_near_by']
+        except KeyError:
+            datahandled['addr_near_by'] = ''
+
+        try:
+            datahandled['addr_postcode'] = postdata['addr_postcode']
+        except KeyError:
+            datahandled['addr_postcode'] = ''
+
+        try:
+            datahandled['floorarea_sqm'] = postdata['floorarea_sqm']
+        except KeyError:
+            datahandled['floorarea_sqm'] = ''
+
+        try:
+            datahandled['geo_latitude'] = postdata['geo_latitude']
+        except KeyError:
+            datahandled['geo_latitude'] = ''
+
+        try:
+            datahandled['geo_longitude'] = postdata['geo_longitude']
+        except KeyError:
+            datahandled['geo_longitude'] = ''
+
+        try:
+            datahandled['property_id'] = postdata['property_id']
+        except KeyError:
+            datahandled['property_id'] = ''
+
+        try:
+            datahandled['post_title_th'] = postdata['post_title_th']
+        except KeyError:
+            datahandled['post_title_th'] = ''
+
+        try:
+            datahandled['post_description_th'] = postdata['post_description_th']
+        except KeyError:
+            datahandled['post_description_th'] = ''
+
+        try:
+            datahandled['post_title_en'] = postdata['post_title_en']
+        except KeyError:
+            datahandled['post_title_en'] = ''
+
+        try:
+            datahandled['post_description_en'] = postdata['post_description_en']
+        except KeyError:
+            datahandled['post_description_en'] = ''
+
+        try:
+            datahandled['ds_id'] = postdata["ds_id"]
+        except KeyError:
+            datahandled['ds_id'] = ''
+
+        try:
+            datahandled['ds_name'] = postdata["ds_name"]
+        except KeyError:
+            datahandled['ds_name'] = ''
+
+        try:
+            datahandled['user'] = postdata['user']
+        except KeyError:
+            datahandled['user'] = ''
+
+        try:
+            datahandled['pass'] = postdata['pass']
+        except KeyError:
+            datahandled['pass'] = ''
+
+        try:
+            datahandled['project_name'] = postdata["project_name"]
+        except KeyError:
+            datahandled['project_name'] = ''
+
+        try:
+            datahandled['bed_room'] = postdata["bed_room"]
+        except KeyError:
+            datahandled['bed_room'] = ''
+
+        try:
+            datahandled['bath_room'] = postdata["bath_room"]
+        except KeyError:
+            datahandled['bath_room'] = ''
+
+        try:
+            datahandled['name'] = postdata["name"]
+        except KeyError:
+            datahandled['name'] = ''
+
+        try:
+            datahandled['mobile'] = postdata["mobile"]
+        except KeyError:
+            datahandled['mobile'] = ''
+
+        try:
+            datahandled['email'] = postdata["email"]
+        except KeyError:
+            datahandled['email'] = ''
+
+        return datahandled
+
+    def create_post(self, postdata):
+        self.print_debug('function ['+sys._getframe().f_code.co_name+']')
+        time_start = datetime.datetime.utcnow()
+
+        # start process
+        #
+        datahandled = self.postdata_handle(postdata)
+        success = "true"
+        detail = ""
+        postid = ""
+
+        #
+        # end process
+
+        time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
+        return {
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "ds_id": datahandled['ds_id'],
+            "post_url": "https://www.ddproperty.com/preview-listing/"+post_id if post_id != "" else "",
+            "post_id": post_id,
+            "account_type": "null",
+            "detail": detail,
         }
 
     def boost_post(self, postdata):
         self.print_debug('function ['+sys._getframe().f_code.co_name+']')
         time_start = datetime.datetime.utcnow()
 
-        post_id = postdata['post_id']
-        log_id = postdata['log_id']
-        email_user = postdata['email_user']
-        email_pass = postdata['email_pass']
+        # start process
+        #
+        datahandled = self.postdata_handle(postdata)
+        success = "true"
+        detail = ""
+        post_id = ""
+        log_id = ""
 
         #
-        #
-        #
+        # end process
 
         time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
         return {
-            "success": "true",
-            "time_usage": time_end - time_start,
-            "time_start": time_start,
-            "time_end": time_end,
-            "detail": "",
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "detail": detail,
             "log_id": log_id,
             "post_id": post_id,
         }
@@ -144,62 +372,50 @@ class websitename():
         self.print_debug('function ['+sys._getframe().f_code.co_name+']')
         time_start = datetime.datetime.utcnow()
 
-        log_id = postdata['post_id']
-        email_user = postdata['email_user']
-        email_pass = postdata['email_pass']
+        # start process
+        #
+        datahandled = self.postdata_handle(postdata)
+        success = "true"
+        detail = ""
+        log_id = ""
 
         #
-        #
-        #
+        # end process
 
         time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
         return {
-            "success": "true",
-            "time_usage": time_end - time_start,
-            "time_start": time_start,
-            "time_end": time_end,
-            "detail": "",
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "detail": detail,
             "log_id": log_id,
         }
 
-    def edit_post(self, postdata, webdata):
+    def edit_post(self, postdata):
         self.print_debug('function ['+sys._getframe().f_code.co_name+']')
         time_start = datetime.datetime.utcnow()
 
-        post_img_url_lists = postdata['post_img_url_lists']
-        price_baht = postdata['price_baht']
-        county = postdata["county"]
-        district = postdata["district"]
-        addr_province = postdata['addr_province']
-        addr_district = postdata['addr_district']
-        addr_sub_district = postdata['addr_sub_district']
-        addr_road = postdata['addr_road']
-        addr_near_by = postdata['addr_near_by']
-        floorarea_sqm = postdata['floorarea_sqm']
-        geo_latitude = postdata['geo_latitude']
-        geo_longitude = postdata['geo_longitude']
-        property_id = postdata['property_id']
-        post_title_th = postdata['post_title_th']
-        post_description_th = postdata['post_description_th']
-        post_title_en = postdata['post_title_en']
-        post_description_en = postdata['post_description_en']
-        post_id = webdata["post_id"]
-        user = webdata["user"]
-        password = webdata["pass"]
-        log_id = webdata["log_id"]
+        # start proces
+        #
+        datahandled = self.postdata_handle(postdata)
+        success = "true"
+        detail = ""
+        log_id = ""
 
         #
-        #
-        #
+        # end process
 
         time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
         return {
-            "success": "true",
-            "time_usage": time_end - time_start,
-            "time_start": time_start,
-            "time_end": time_end,
-            "detail": "",
-            "log_id": ""
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            "detail": detail,
+            "log_id": log_id
         }
 
     def print_debug(self, msg):
