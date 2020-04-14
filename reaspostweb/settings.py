cf87a87433067286e039acb89755029551c45e09
Panuwat Ruangrak <vinvestor.panuwat@gmail.com>
2020-04-14 13:51:47 +0000

    Add server 209.97.168.205

--- reaspostweb/settings.py
+++ reaspostweb/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['192.168.2.40', '192.168.1.56', 'localhost', '127.0.0.1',
-                 '192.168.42.62', '178.128.212.153', 'homepostapi.webup.site', 'homepostapi-dev.webup.site']
+                 '192.168.42.62', '178.128.212.153', 'homepostapi.webup.site', 'homepostapi-dev.webup.site', '209.97.168.205']
 
 
 # Application definition
