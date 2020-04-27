72406c0f4c1a26662da0e8df43ce5dc400b749b1
arnut klamhui <kla.arnut@gmail.com>
2020-04-28 02:38:55 +0700

    update

--- reaspostweb/urls.py
+++ reaspostweb/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-
-
 from reaspostweb import view as reaspostweb_view
 
 urlpatterns = [
