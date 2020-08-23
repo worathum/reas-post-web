9cdfcb2e7e9d032d10ff800dbd88e76b952573ae
Priyanshu Madaan <priyanshu.madaan@students.iiit.ac.in>
2020-08-23 12:16:27 +0530

    prop added proxy

--- webmodule/proptecheasy.py
+++ webmodule/proptecheasy.py
@@ -253,22 +253,22 @@ def upload_image(img_add,security_code):
 
 httprequestObj = lib_httprequest()
 
-# with open("./static/ips.txt",'r') as f:
-#     allips = f.read()
-# ips = allips.split('\n')
+with open("./static/ips.txt",'r') as f:
+    allips = f.read()
+ips = allips.split('\n')
 
-# username = ips[random.randint(0,len(ips)-2)].split(":")[2]
-# password = 'v1y3mbh26qk9'
-# port = 22225
-# super_proxy_url = ('http://%s:%s@zproxy.lum-superproxy.io:%d' %
-#         (username, password, port))
+username = ips[random.randint(0,len(ips)-2)].split(":")[2]
+password = 'v1y3mbh26qk9'
+port = 22225
+super_proxy_url = ('http://%s:%s@zproxy.lum-superproxy.io:%d' %
+        (username, password, port))
 
-# proxy_handler = {
-#     'http': super_proxy_url,
-#     'https': super_proxy_url,
-# }
+proxy_handler = {
+    'http': super_proxy_url,
+    'https': super_proxy_url,
+}
 
-# httprequestObj.session.proxies.update(proxy_handler)
+httprequestObj.session.proxies.update(proxy_handler)
 
 
 
