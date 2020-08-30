719824c96038424cc0566a2f99682ffd53943d78
Priyanshu Madaan <priyanshu.madaan@students.iiit.ac.in>
2020-08-30 14:47:57 +0530

    fix dd

--- webmodule/ddproperty.py
+++ webmodule/ddproperty.py
@@ -208,7 +208,7 @@ class ddproperty():
         options = Options()
         # debug by comment option --headless
         options.add_argument("--headless")
-        # options.add_argument('--no-sandbox')
+        options.add_argument('--no-sandbox')
         options.add_argument('start-maximized')
         options.add_argument('disable-infobars')
         options.add_argument("--disable-extensions")
