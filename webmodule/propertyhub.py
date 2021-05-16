9f8972e2a2db80618e0ab8b28d08b713fca5022c
patcharin <patcharinw.1212@gmail.com>
2021-05-16 19:01:48 +0700

    fix image-propertyhub

--- webmodule/propertyhub.py
+++ webmodule/propertyhub.py
@@ -277,15 +277,19 @@ class propertyhub():
 
                     body = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'body'))).send_keys(Keys.PAGE_DOWN)
 
-                    all_images = ""
+                    upload = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div[1]/div[1]/div/form/div/div/div[3]/div[2]/input')))
+                    #all_images = ""
                     for count, pic in enumerate(postdata['post_images']):
                         if count < len(postdata['post_images'])-1:
-                            all_images += os.path.abspath(pic) + '\n'
+                            pics = os.path.abspath(pic)+ '\n'
+                            upload.send_keys(pics)
+                            #all_images += os.path.abspath(pic) + '\n'
                         else:
-                            all_images += os.path.abspath(pic)
+                            last_pic = os.path.abspath(pic)
+                            upload.send_keys(last_pic)
                     
-                    upload = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div[1]/div[1]/div/form/div/div/div[3]/div[2]/input')))
-                    upload.send_keys(all_images)
+                    #upload = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__next"]/div[1]/div[1]/div/form/div/div/div[3]/div[2]/input')))
+                    #upload.send_keys(all_images)
         
                     detail_post = WebDriverWait(self.driver, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'ql-editor')))
                     detail_post[1].click()
