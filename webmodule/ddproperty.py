d35e99c7e427f98fb96018e07afcae18caa6464d
Mai Chinu <chisanupolwo@gmail.com>
2021-06-09 00:07:12 +0700

    Headless activate ddproperty"

--- webmodule/ddproperty.py
+++ webmodule/ddproperty.py
@@ -207,7 +207,7 @@ class ddproperty():
 
         options = Options()
         # debug by comment option --headless
-        #options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument('--no-sandbox')
         options.add_argument('start-maximized')
         options.add_argument('disable-infobars')
