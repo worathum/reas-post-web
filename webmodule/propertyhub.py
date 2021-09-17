45991a478a838348bdb59b9f491ddcad3d686c82
worathum <worathum.h@gmail.com>
2021-09-17 13:45:55 +0700

    Update propertyhub.py

--- webmodule/propertyhub.py
+++ webmodule/propertyhub.py
@@ -20,7 +20,7 @@ class propertyhub():
     def test_login(self, postdata):
         
         options = Options()
-        #options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('start-maximized')
         options.add_argument('disable-infobars')
