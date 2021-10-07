59d8975360daf4017e3f707e8a3b11d957982de0
fangchunxi1999 <fangchunxi19990113@gmail.com>
2021-10-07 15:46:30 +0700

    Add ddproperty_plus.py by inherit from ddproperty.py

---
+++ webmodule/ddproperty_plus.py
@@ -0,0 +1,9 @@
+from .ddproperty import ddproperty
+
+
+class ddproperty_plus(ddproperty):
+    name = "ddproperty_plus"
+
+    def __init__(self):
+        super().__init__()
+        self.websitename = "ddproperty_plus"
