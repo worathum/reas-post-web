a1d1a9a503c7c9c3106154dd609ea81419bd90cf
worathum <worathum.h@gmail.com>
2021-11-17 14:46:02 +0700

    Update livinginsider.py

--- webmodule/livinginsider.py
+++ webmodule/livinginsider.py
@@ -149,8 +149,6 @@ class livinginsider():
         
         for key in zone_data:
             if zone in zone_data[key]['Zone name \u0e44\u0e17\u0e22']:
-                print(zone)
-                print(zone_data[key]['Zone name \u0e44\u0e17\u0e22'])
                 web_zone_id = key
                 break
         with open('./static/living_project_id.json') as f:
@@ -273,7 +271,7 @@ class livinginsider():
             data['web_project_id'] = int(project_id)
             data['web_latitude'] = postdata['geo_latitude']
             data['web_longitude'] = postdata['geo_longitude']
- 
+        
         if action == 'edit':
             data['web_id'] = int(postdata['post_id'])
         
@@ -283,7 +281,7 @@ class livinginsider():
             success = True
         else:
             detail = 'Cannot found zone id'
-
+            response = ''
         return {
             'success': success,
             'detail': detail,
@@ -313,8 +311,6 @@ class livinginsider():
                 }
                 r = self.httprequestObj.http_post('{}/profile.php'.format(self.webbaseurl), data=data)
                 response = r.json()
-                print(response)
-                print(response['profile']['credit_balance'])
                 if str(response['result_code']) == '0':
                     if int(response['profile']['credit_balance'])>= 20:
                         url = '{}/post_add.php'.format(self.webbaseurl)
@@ -455,15 +451,90 @@ class livinginsider():
     def boost_post(self, postdata):
         self.print_debug('function [' + sys._getframe().f_code.co_name + ']')
         time_start = datetime.datetime.utcnow()
-        
-        success = False
-        detail = 'Something wrong'
-        edit = self.edit_post(postdata)
-        success = edit['success']
+
+        post_id = postdata['post_id']
+        log_id = postdata['log_id']
+
+        test_login = self.test_login(postdata)
+        success = test_login["success"]
+        detail = ""
+
         if success:
-            detail = 'Boost successful'
+
+            headers = {
+                    'Connection': 'keep-alive',
+                    'Upgrade-Insecure-Requests': '1',
+                    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Mobile Safari/537.36',
+                    'Sec-Fetch-Mode': 'navigate',
+                    'Sec-Fetch-User': '?1',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                    'Sec-Fetch-Site': 'same-origin',
+                    'Referer': 'https://www.livinginsider.com/mystock.php',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+                }
+            res = self.httprequestObj.http_get('https://www.livinginsider.com/living_edit.php', params={'topic_id': str(post_id)}, headers=headers)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            #print(soup.find('meta', {'name': 'csrf-token'}).get('content'))
+            csrf_token = soup.find('meta', {'name': 'csrf-token'}).get('content')
+            referer = 'https://www.livinginsider.com/living_edit.php?topic_id=' + str(postdata['post_id']) + '&currentID=' + str(postdata['post_id'])
+
+            headers = {
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'th-TH,th;q=0.9,en;q=0.8',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Host': 'www.livinginsider.com',
+                'Origin': 'https://www.livinginsider.com',
+                'Referer': referer,
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36', 
+                'X-CSRF-TOKEN': csrf_token,
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+
+            r = self.httprequestObj.http_post('https://www.livinginsider.com/a_edit_living.php', data=None, headers=headers)
+            res = self.httprequestObj.http_get('https://www.livinginsider.com/living_edit2.php?topic_id='+ postdata['post_id'], headers=headers)
+            
+            headers = {
+                'Connection': 'keep-alive',
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'X-Requested-With': 'XMLHttpRequest',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Origin': 'https://www.livinginsider.com',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Dest': 'empty',
+                'Referer': 'https://www.livinginsider.com/living_edit2.php?topic_id=' + post_id,
+                'Accept-Language': 'en-IN,en-US;q=0.9,en;q=0.8',
+            }
+
+            r = self.httprequestObj.http_post('https://www.livinginsider.com/a_edit_living.php', headers=headers, data=None)
+
+            data = {
+                'hidden_status': '',
+                'action': 'save',
+                'web_status': '1',
+                'publish_flag': '0',
+                'state_renew': ''
+            }
+
+            r = self.httprequestObj.http_post('https://www.livinginsider.com/living_edit_confirm.php', params={'topic_id': post_id}, data=data)
+
+            if r.status_code == 200:
+                success = 'true'
+                detail = 'Post was boosted was successfully.'
+            else:
+                success = 'false'
+                detail = 'Can not boost the post.'
+            
         else:
-            detail = edit['detail']
+            success = False
+            detail = 'Couldnot login'
 
         time_end = datetime.datetime.utcnow()
         return {
@@ -472,9 +543,9 @@ class livinginsider():
             "start_time": time_start,
             "end_time": time_end,
             "detail": detail,
-            "log_id": postdata['log_id'],
+            "log_id": log_id,
             "ds_id": postdata['ds_id'],
-            "post_id": postdata['post_id'],
+            "post_id": post_id,
             "websitename": "livinginsider",
             "post_view": ''
         }
