edb18ec21eb29e287dfc6558cc426cf11b25ab8d
Tanakon.vinvestor <tanakon@vinvestor.co.th>
2021-12-07 16:19:28 +0700

    login, logout, register look good

--- webmodule/trovit.py
+++ webmodule/trovit.py
@@ -1,6 +1,6 @@
 from .lib_httprequest import *
 import datetime
-import re
+import sys
 
 try:
     import configs
@@ -13,76 +13,60 @@ class trovit():
 
     def __init__(self):
 
-        self.websitename = 'trovit'
+        try:
+            import configs
+
+        except ImportError:
+            configs = {}
+
+        self.httprequestObj = lib_httprequest()
         self.encoding = 'utf-8'
         self.imgtmp = 'imgtmp'
+        self.debug = 1
+        self.debugresdata = 0
         self.parser = 'html.parser'
-        self.handled = False
-        self.httprequestObj = lib_httprequest()
+        self.webname = 'trovit'
+
+    def print_debug(self, msg):
+        if self.debug == 1:
+            print(msg)
+        return True
+
+    def logout_user(self):
+        url = "https://th.trovit.com/baan/index.php/cod.logout"
+        self.httprequestObj.http_get(url)
+
+    def register_details(self, postdata):
+        register_data = {}
+
+        register_data["email"] = postdata["user"]
+        register_data["password"] = postdata["pass"]
+        register_data["password2"] = postdata["pass"]
+        register_data["terms"] = "on"
+
+        return register_data
 
     def register_user(self, postdata):
-        #log.debug('')
-
+        self.logout_user()
+        self.print_debug('function [' + sys._getframe().f_code.co_name + ']')
         time_start = datetime.datetime.utcnow()
 
         # start process
         #
-        datahandled = self.postdata_handle(postdata)
-        user = datahandled['user']
-        passwd = datahandled['pass']
-        company_name = postdata['company_name']
-        name_title = postdata["name_title"]
-        name_th = postdata["name_th"]
-        surname_th = postdata["surname_th"]
-       
-        tel = postdata["tel"]
-        line = postdata["line"]
-        tel = list(tel)
-        del tel[0]
-        newtel = ''.join(tel)
-
-        datapost = {
-            'agency_id': 'OTHER',
-            'otheragency-th-text': company_name,
-            'otheragency-en-text': company_name,
-            'otheragency': '',
-            'job_title-th-text': '',
-            'job_title-en-text': '',
-            'job_title': '',
-            'title': name_title,
-            'firstname-th-text': name_th,
-            'firstname-en-text': '',
-            'firstname': '',
-            'lastname-th-text': surname_th,
-            'lastname-en-text': '',
-            'lastname': '',
-            'birthDay': 10,
-            'birthMonth': 10,
-            'birthYear': 1986,
-            'email': user,
-            'mobile': newtel,
-            'region': 'TH37',
-            'city_area': '',
-            'password': passwd,
-            'password_confirm': passwd,
-            'communication_us': 1,
-            'submit': 'Submit',
-            'months': ''
-        }
+        data_register = self.register_details(postdata)
 
 
-        r = self.httprequestObj.http_post('https://accounts.trovit.com/?cod=create_user&url=https%3A%2F%2Fth.trovit.com%2Fbaan%2Findex.php%2Fcod.mail_preferences&language=th_TH&', data=datapost)
-        data = r.text
-        # f = open("debug_response/trovregister.html", "wb")
-        # f.write(data.encode('utf-8').strip())
-        
+        res = self.httprequestObj.http_post('https://accounts.trovit.com/?cod=create_user&url=https%3A%2F%2Fth.trovit.com%2Fbaan%2Findex.php%2Fcod.mail_preferences&language=th_TH&', data=data_register)
+
         detail = ""
         register_success = False
-        check_register = "<title>ส่งอีเมลยืนยันแล้ว - Trovit</title>"
-        soup_web = bool(BeautifulSoup(data, "html.parser").find())
+        soup_web = BeautifulSoup(res.text, "html5lib")
         if soup_web:
-            register_success = bool(BeautifulSoup(check_register, "html.parser").find())
-        #
+            verify_register = soup_web.find("p", attrs={"class":"before_form"}).text
+            if verify_register == "อีเมลนี้ใช้ในการสมัครแล้ว คุณสามารลงชื่อเข้าใช้ได้ที่นี่":
+                register_success = True
+
+        # 
         # end process
 
         time_end = datetime.datetime.utcnow()
@@ -95,6 +79,41 @@ class trovit():
             "end_time": str(time_end),
             "detail": detail,
         }
+
+    def test_login(self, postdata):
+        self.logout_user()
+        self.print_debug('function [' + sys._getframe().f_code.co_name + ']')
+        url = "https://accounts.trovit.com/?cod=check_login&url=https%3A%2F%2Fth.trovit.com%2Fbaan%2Findex.php%2Fcod.mail_preferences&language=th_TH&"
+
+        time_start = datetime.datetime.utcnow()
+
+        data_login = {
+            'email' : postdata['user'],
+            'password' : postdata['pass']
+        }
+
+        r = self.httprequestObj.http_post(url, data=data_login)
+        print(r.status_code)
+
+        detail = ""
+        success = False
+        soup = BeautifulSoup(r.text, self.parser)
+        verify_login = soup.find("span", attrs={"class":"account-button-text header-option-button-text"}).text
+        if verify_login == "บัญชี":
+            success = True
+
+        time_end = datetime.datetime.utcnow()
+        time_usage = time_end - time_start
+        return {
+            "success": success,
+            "usage_time": str(time_usage),
+            "start_time": str(time_start),
+            "end_time": str(time_end),
+            'ds_id': postdata['ds_id'],
+            "detail": detail,
+            "websitename": self.webname,
+        }
+
 
     def postdata_handle(self, postdata):
         #log.debug('')
