c3ac602c313853778147b06bdf542e2fcd35e0cf
worathum h <worathum.h@gmail.com>
2021-12-27 13:26:24 +0700

    save

--- webmodule/proppit.py
+++ webmodule/proppit.py
@@ -270,7 +270,7 @@ class proppit():
         path = './static/chromedriver'
         options = Options()
 
-        #options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument('--no-sandbox')
         options.add_argument('start-maximized')
         options.add_argument('disable-infobars')
